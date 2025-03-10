0899f41166c5e12e792540648790ff20d900a127
Debora Bimbi <deborabimbi1985@gmail.com>
2025-03-10 15:28:37 +0100

    first commit

---
+++ solutions.sql
@@ -0,0 +1,134 @@
+--- Which tables to use
+
+USE bank;
+
+--- Query 1
+
+SELECT client_id
+FROM client
+WHERE district_id = 1
+LIMIT 5;
+
+--- Query 2
+
+SELECT client_id
+FROM client
+where district_id = 72
+ORDER BY client_id DESC
+limit 1;
+
+
+--- Query 3
+
+SELECT amount
+FROM loan
+ORDER BY amount ASC
+LIMIT 3;
+
+--- Query 4
+
+SELECT DISTINCT status
+FROM loan
+ORDER BY status ASC;
+
+--- Query 5
+
+SELECT loan_id
+FROM loan
+ORDER BY payments DESC
+LIMIT 1;
+
+--- Query 6
+
+SELECT account_id as '#id', amount
+FROM loan
+ORDER BY account_id ASC
+LIMIT 5;
+
+--- Query 7
+
+SELECT account_id
+FROM loan
+WHERE duration = 60
+ORDER BY amount ASC
+LIMIT 5;
+
+--- Query 8
+
+SELECT DISTINCT k_symbol
+FROM `order`
+ORDER BY k_symbol;
+
+--- Query 9
+
+SELECT order_id
+FROM `order`
+WHERE account_id = 34;
+
+--- Query 10
+
+SELECT DISTINCT account_id
+FROM `order`
+WHERE order_id BETWEEN 29540 and 29560;
+
+--- Query 11
+
+SELECT amount
+FROM `order`
+WHERE account_to = 30067122;
+
+--- Query 12
+
+SELECT trans_id, date, type, amount
+FROM trans
+WHERE account_id = 793
+ORDER BY date DESC
+LIMIT 10;
+
+--- Query 13
+
+SELECT district_id, COUNT(DISTINCT client_id)
+FROM client
+WHERE district_id < 10
+GROUP BY district_id;
+
+--- Query 14
+
+SELECT DISTINCT type, COUNT(card_id)
+FROM card
+GROUP BY type
+ORDER BY COUNT(card_id) DESC;
+
+--- Query 15
+
+SELECT account_id, SUM(amount)
+FROM loan
+ORDER BY SUM(amount) DESC
+LIMIT 10;
+
+--- Query 16
+
+SELECT date, COUNT(loan_id)
+FROM loan
+WHERE date < 930907
+GROUP BY date
+ORDER BY date DESC;
+
+
+--- Query 17
+
+SELECT date, duration, COUNT(loan_id)
+FROM loan
+WHERE date LIKE '9712%'
+GROUP BY date, duration
+##HAVING COUNT(loan_id) > 0 not needed due to group by
+ORDER BY date ASC, duration ASC;
+
+
+--- Query 18
+
+SELECT account_id, type, sum(amount) as total_amount
+FROM trans
+WHERE account_id = 396
+GROUP BY account_id, type
+ORDER BY type ASC;
